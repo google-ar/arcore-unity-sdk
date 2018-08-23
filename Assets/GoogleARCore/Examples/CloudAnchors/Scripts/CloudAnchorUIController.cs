801c4853d9ea50e3ef027a51b6e67811e526e20b
Pablo Rodriguez Manzi <pmanzi@google.com>
2018-08-23 07:19:18 -0700

    ARCore Unity SDK v1.4.1

--- Assets/GoogleARCore/Examples/CloudAnchors/Scripts/CloudAnchorUIController.cs
+++ Assets/GoogleARCore/Examples/CloudAnchors/Scripts/CloudAnchorUIController.cs
@@ -20,10 +20,7 @@
 
 namespace GoogleARCore.Examples.CloudAnchors
 {
-    using System.Collections.Generic;
-    using GoogleARCore;
-    using GoogleARCore.CrossPlatform;
-    using GoogleARCore.Examples.Common;
+    using System.Net;
     using UnityEngine;
     using UnityEngine.UI;
 
@@ -82,7 +79,7 @@ namespace GoogleARCore.Examples.CloudAnchors
         /// </summary>
         public void Start()
         {
-            IPAddressText.text = "My IP Address: " + Network.player.ipAddress;
+            IPAddressText.text = "My IP Address: " + _GetDeviceIpAddress();
         }
 
         /// <summary>
@@ -233,6 +230,31 @@ namespace GoogleARCore.Examples.CloudAnchors
         public void OnResolveOnDeviceValueChanged(bool isResolveOnDevice)
         {
             IpAddressInputField.interactable = !isResolveOnDevice;
+        }
+
+        /// <summary>
+        /// Gets the device ip address.
+        /// </summary>
+        /// <returns>The device ip address.</returns>
+        private string _GetDeviceIpAddress()
+        {
+            string ipAddress = "Unknown";
+#if UNITY_2018_2_OR_NEWER
+            string hostName = Dns.GetHostName();
+            IPAddress[] addresses = Dns.GetHostAddresses(hostName);
+
+            foreach (IPAddress address in addresses)
+            {
+                if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    ipAddress = address.ToString();
+                    break;
+                }
+            }
+#else
+            ipAddress = Network.player.ipAddress;
+#endif
+            return ipAddress;
         }
     }
 }
